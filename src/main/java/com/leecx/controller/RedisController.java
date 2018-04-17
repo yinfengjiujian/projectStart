1763ea0ee8354ede7aa533c8f016b691e4d4bc3e
yinfengjiujian <duanmeilin911@163.com>
2018-04-17 23:57:07 +0800

    提交redis集成测试类

--- src/main/java/com/leecx/controller/RedisController.java
+++ src/main/java/com/leecx/controller/RedisController.java
@@ -1,30 +1,33 @@
 package com.leecx.controller;
 
 import com.leecx.pojo.LeeJSONResult;
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/error")
+/**
+ * <p>Title:</p>
+ * <p>Description:redis测试类</p>
+ * <p>Copyright:Copyright (c) 2018</p>
+ * <p>Company:东软集团股份有限公司</p>
+ * CreateDate:2018/4/17 0017 下午 23:44
+ * Author:段美林[duanml@neusoft.com]
+ * Version:1.0
+ */
+@RestController
+@RequestMapping("/redis")
 public class RedisController {
+
+	@Autowired
+	private StringRedisTemplate strRedis;
 	
-	@RequestMapping("/ajaxerror")
-	public String ajaxError() {
-		return "thymeleaf/ajaxerror";
-	}
-	
-	@RequestMapping("/getAjaxerror")
-	@ResponseBody
-	public LeeJSONResult getAjaxError() {
-		System.out.println("=================");
-//		try {
-			int a = 1 / 0;
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			return LeeJSONResult.errorException(e.getMessage());
-//		}
-		return LeeJSONResult.ok();
+	@RequestMapping("/test")
+	public LeeJSONResult testRedis() {
+
+		strRedis.opsForValue().set("test RedisController","hello test RedisController!!!");
+
+		return LeeJSONResult.ok(strRedis.opsForValue().get("test RedisController"));
 	}
 
 }
